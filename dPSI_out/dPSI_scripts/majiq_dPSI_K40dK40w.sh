7c359ed310c4693986732245f0b327536a3b5c9d
hasnakhan <hasna.khan@mail.utoronto.ca>
2022-01-25 15:48:59 -0500

    re-added dPSI scripts

---
+++ dPSI_out/dPSI_scripts/majiq_dPSI_K40dK40w.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+majiq deltapsi -j 3 -o /mnt/c/Users/hasna/Documents/MSc_project/majiq/dPSI_out/K40dK40w -grp1 /mnt/c/Users/hasna/Documents/MSc_project/majiq/build_out/D7K40d1_sorted.majiq /mnt/c/Users/hasna/Documents/MSc_project/majiq/build_out/D7K40d2_sorted.majiq -grp2 /mnt/c/Users/hasna/Documents/MSc_project/majiq/build_out/D7K40W2_sorted.majiq /mnt/c/Users/hasna/Documents/MSc_project/majiq/build_out/D7K40W3_sorted.majiq /mnt/c/Users/hasna/Documents/MSc_project/majiq/build_out/D7K40W4_sorted.majiq -n K40d K40w
