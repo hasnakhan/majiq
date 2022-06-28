a3d804cb8f9d62928266597b8cf13a76b37d7cac
hasnakhan <hasna.khan@mail.utoronto.ca>
2022-06-28 13:48:11 -0400

    run phastcons on all sequences associated with alt introns

---
+++ dAS_analysis/conservation/phastCons/alternative/alt_phastcons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for folder in /mnt/c/Users/hasna/Documents/MSc_project/majiq/dAS_analysis/conservation/phastCons/alternative/*; 
+do
+	for sequence in /mnt/c/Users/hasna/Documents/MSc_project/majiq/dAS_analysis/conservation/phastCons/alternative/"${folder##*/}"/*.fasta; 
+	do
+	name=$(basename "$sequence")
+	type=$(basename "$folder")
+	#echo "$folder"
+	#echo "$sequence"
+	phastCons --estimate-trees $folder/trees/$name $sequence phyloFit.mod > $folder/"${name##*/}.wig"
+	done
+done
